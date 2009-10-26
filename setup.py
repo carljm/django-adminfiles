43b0eb37451254dfd3a59210f9900a88ee10c25d
Carl Meyer <carl@dirtcircle.com>
2009-10-26 18:25:48 -0400

    setup.py updates

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-admin-uploads',
-    version='0.1.0',
+    version='0.2.0',
     description='file upload manager and picker for Django admin',
     author='sgt.hulka',
     author_email='sgt.hulka@gmail.com',
     url='http://code.google.com/p/django-admin-uploads/',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -17,7 +17,8 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-    include_package_data=True,
     zip_safe=False,
-    install_requires=['setuptools'],
+    package_data={'upload': ['media/upload/*.*',
+                             'media/upload/mimetypes/*.png',
+                             'templates/upload/*.html']}
 )
