beab509b8506fe98af65c1ab9f36b7331e979f77
Carl Meyer <carl@dirtcircle.com>
2009-11-23 22:59:09 -0500

    bump version for release

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ long_description = (open('README.txt').read() +
 
 setup(
     name='django-adminfiles',
-    version='0.3.0dev%s' % hgrev,
+    version='0.3.0',
     description='File upload manager and picker for Django admin',
     author='sgt.hulka',
     author_email='sgt.hulka@gmail.com',
