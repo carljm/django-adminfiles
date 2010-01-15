1b3ddf73c780c01774c10be1bbca83282454f1a4
Carl Meyer <carl@dirtcircle.com>
2010-01-15 14:25:31 -0500

    upgrade to jQuery 1.4

--- adminfiles/settings.py
+++ adminfiles/settings.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 JQUERY_URL = getattr(
     settings, 'JQUERY_URL',
-    'http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js')
+    'http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js')
 
 if not ((':' in JQUERY_URL) or (JQUERY_URL.startswith('/'))):
     JQUERY_URL = posixpath.join(settings.MEDIA_URL, JQUERY_URL)
